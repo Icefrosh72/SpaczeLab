9703ccc7a90aa820687565b2f0d3ab90ab1533aa
Seidu Tesleem <67004153+Icefrosh72@users.noreply.github.com>
2024-06-06 14:18:40 +0000

    features adding up

--- SpaczeLab/src/component/HeroSection.jsx
+++ SpaczeLab/src/component/HeroSection.jsx
@@ -13,22 +13,45 @@ const HeroSection = () => {
             for developers
           </div>
         </h1>
-        <p className="mt-10 text-lg text-center text-neutral-500 max-w-4x-l"> 
+        <p className="mt-10 text-lg text-center text-neutral-500 max-w-4x-l">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum iste
           aliquid quo numquam quas non quidem neque. Dolore, eos consequuntur!
           Quae velit vitae animi et doloremque, aspernatur ipsum harum? Minus.
         </p>
         <div className="flex justify-center my-10">
-          <a href="#" className="bg-gradient-to-r from-purple-500 to-blue-800 py-3 px-4 mx-3 round-md">
+          <a
+            href="#"
+            className="bg-gradient-to-r from-purple-500 to-blue-800 py-3 px-4 mx-3 round-md"
+          >
             Get Started
           </a>
           <a href="#" className=" py-3 px-4 mx-3 round-md border">
             Documentation
           </a>
         </div>
+        <div className="flex mt-10 justify-center">
+          <video
+            autoPlay
+            loop
+            muted
+            className="rounded-lg w-1/2 border border-blue-700 shadow-purple-400 mx-2 my-4"
+          >
+            <source src={video1} type="video/mp4" />
+            Your browser does not support the code
+          </video>
+          <video
+            autoPlay
+            loop
+            muted
+            className="rounded-lg w-1/2 border border-blue-700 shadow-purple-400 mx-2 my-4"
+          >
+            <source src={video2} type="video/mp4" />
+            Your browser does not support the code
+          </video>
+        </div>
       </div>
     </>
   );
 };
 
-export default HeroSection
+export default HeroSection;
