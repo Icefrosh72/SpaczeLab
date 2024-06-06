8ce353869a058ed76979e90a858efe29b5f41f41
Seidu Tesleem <67004153+Icefrosh72@users.noreply.github.com>
2024-06-06 13:42:13 +0000

    improving interface

--- SpaczeLab/src/component/HeroSection.jsx
+++ SpaczeLab/src/component/HeroSection.jsx
@@ -1,21 +1,34 @@
-import React from 'react';
-import video1 from "../assets/video1.mp4"
-import video2 from "../assets/video2.mp4"
+import React from "react";
+import video1 from "../assets/video1.mp4";
+import video2 from "../assets/video2.mp4";
 
 const HeroSection = () => {
   return (
     <>
-        <div className="flex flex-col items-center mt-6 lg:mt-20">
-            <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
-                SpaczeLab build tools
-                <div className="bg-gradient-to-r from-purple-500 to-blue-800 text-transparent bg-clip-text">
-                    {""}
-                    for developers
-                </div>
-            </h1>
+      <div className="flex flex-col items-center mt-6 lg:mt-20">
+        <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
+          SpaczeLab build tools
+          <div className="bg-gradient-to-r from-purple-500 to-blue-800 text-transparent bg-clip-text">
+            {""}
+            for developers
+          </div>
+        </h1>
+        <p className="mt-10 text-lg text-center text-neutral-500 max-w-4x-l"> 
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum iste
+          aliquid quo numquam quas non quidem neque. Dolore, eos consequuntur!
+          Quae velit vitae animi et doloremque, aspernatur ipsum harum? Minus.
+        </p>
+        <div className="flex justify-center my-10">
+          <a href="#" className="bg-gradient-to-r from-purple-500 to-blue-800 py-3 px-4 mx-3 round-md">
+            Get Started
+          </a>
+          <a href="#" className=" py-3 px-4 mx-3 round-md border">
+            Documentation
+          </a>
         </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default HeroSection;
+export default HeroSection
